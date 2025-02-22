c579e746a95eff76efff54a30c4134f577eec2b8
xensen008 <arnabjyotikakati008@gmail.com>
2025-02-22 09:29:54 +0530

    Add mobile navigation and Navbar for larger screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,28 @@
+'use client'
 import React from 'react'
-
+import Link from 'next/link'
+import Image from 'next/image'
+import MobileNav from './MobileNav'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex-between fixed z-50  w-full bg-dark-1 px-6 py-4 lg:px-10  '>
+      <Link href='/' className='flex items-center gap-1'>
+        <Image
+          src='/icons/logo.svg'
+          alt='logo'
+          width={42}
+          height={42}
+          className='max-sm:size-10'
+        />
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Meetly</p>
+      </Link>
+
+      <div className='flex-between gap-5'>
+        {/* {cleark user management } */}
+        <MobileNav/>
+      </div>
+
+    </nav>
   )
 }
 
