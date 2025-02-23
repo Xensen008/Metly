fc679b7eb02b4c86a01591f4fc31684d998d188c
xensen008 <arnabjyotikakati008@gmail.com>
2025-02-24 03:21:24 +0530

    Add video call functionality with hooks (meeting page setup and room) and components; update global styles and metadata

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,14 @@
 import React, { ReactNode } from 'react'
 import StreamVideoProvider from '../../../providers/streamClientProvider'
 import { Toaster } from "@/components/ui/sonner"
-
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: "Meetly",
+  description: "Meetly is a video conferencing app built with Next.js and Stream Chat.",
+  icons:{
+    icon:"/icons/logo.svg",
+  }
+};
 const RootLayout = ({children}: {children:ReactNode}) => {
   return (
     <main>
