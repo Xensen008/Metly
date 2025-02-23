1154fd19708bf8a7fbdf576da2252ced7caec114
xensen008 <arnabjyotikakati008@gmail.com>
2025-02-23 19:38:19 +0530

    Meetingmodel  updated and stream added

--- src/components/MeetingModal.tsx
+++ src/components/MeetingModal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTitle,DialogDescription } from "@/components/ui/dialog";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
@@ -30,6 +30,12 @@ const MeetingModal = ({
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
             <DialogContent className="flex w-full rounded-2xl max-w-[520px] flex-col gap-6 bg-dark-1 border-none px-6 py-9 text-white">
+                <DialogTitle className="sr-only">
+                    {title}
+                </DialogTitle>
+                <DialogDescription className="sr-only">
+                    {title} dialog content
+                </DialogDescription>
                 <div className="flex flex-col gap-6">
                     {image && (
                         <div className="flex justify-center">
@@ -38,7 +44,10 @@ const MeetingModal = ({
                     )}
                     <h1 className={cn('text-3xl font-bold leading-[42px]', classname)}>{title}</h1>
                     {children}
-                    <Button className="bg-blue-1 rounded-xl focus-visible:ring-0 focus-visible:ring-offset-0" onClick={handleClick}>
+                    <Button 
+                        className="bg-blue-1 rounded-xl focus-visible:ring-0 focus-visible:ring-offset-0" 
+                        onClick={handleClick}
+                    >
                         {buttonIcon && <Image src={buttonIcon} alt="Button Icon" width={13} height={13} />} &nbsp;
                         {buttonText || "Schedule a Meeting"}
                     </Button>
