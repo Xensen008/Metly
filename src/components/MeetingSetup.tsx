10a4d5fbdd354876cf2bd43289b28a16a7524163
xensen008 <arnabjyotikakati008@gmail.com>
2025-02-27 14:09:16 +0530

    Enhance MeetingSetup component to disable camera and microphone when the meeting hasn't started, and improve toggle logic for mic/cam functionality.

--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -15,34 +15,35 @@ const MeetingSetup = ({
 }: {
   setIsSetupComplete: (value: boolean) => void;
 }) => {
-  // https://getstream.io/video/docs/react/guides/call-and-participant-state/#call-state
   const { useCallEndedAt, useCallStartsAt } = useCallStateHooks();
   const callStartsAt = useCallStartsAt();
   const callEndedAt = useCallEndedAt();
-  const callTimeNotArrived =
-    callStartsAt && new Date(callStartsAt) > new Date();
+  const callTimeNotArrived = callStartsAt && new Date(callStartsAt) > new Date();
   const callHasEnded = !!callEndedAt;
 
   const call = useCall();
-
-  if (!call) {
-    throw new Error(
-      'useStreamCall must be used within a StreamCall component.',
-    );
-  }
-
-  // https://getstream.io/video/docs/react/ui-cookbook/replacing-call-controls/
   const [isMicCamToggled, setIsMicCamToggled] = useState(false);
 
+  // Disable camera and mic when meeting hasn't started
   useEffect(() => {
-    if (isMicCamToggled) {
-      call.camera.disable();
-      call.microphone.disable();
-    } else {
-      call.camera.enable();
-      call.microphone.enable();
+    if (callTimeNotArrived) {
+      call?.camera.disable();
+      call?.microphone.disable();
     }
-  }, [isMicCamToggled, call.camera, call.microphone]);
+  }, [callTimeNotArrived, call]);
+
+  // Regular mic/cam toggle logic
+  useEffect(() => {
+    if (!callTimeNotArrived) {
+      if (isMicCamToggled) {
+        call?.camera.disable();
+        call?.microphone.disable();
+      } else {
+        call?.camera.enable();
+        call?.microphone.enable();
+      }
+    }
+  }, [isMicCamToggled, call?.camera, call?.microphone, callTimeNotArrived]);
 
   if (callTimeNotArrived)
     return (
@@ -62,7 +63,7 @@ const MeetingSetup = ({
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
       <h1 className="text-center text-2xl font-bold">Setup</h1>
-      <VideoPreview />
+      {!callTimeNotArrived && <VideoPreview />}
       <div className="flex h-16 items-center justify-center gap-3">
         <label className="flex items-center justify-center gap-2 font-medium">
           <input
@@ -72,13 +73,12 @@ const MeetingSetup = ({
           />
           Join with mic and camera off
         </label>
-        <DeviceSettings />
+        {!callTimeNotArrived && <DeviceSettings />}
       </div>
       <Button
         className="rounded-xl bg-green-500 px-4 py-2.5"
         onClick={() => {
-          call.join();
-
+          call?.join();
           setIsSetupComplete(true);
         }}
       >
